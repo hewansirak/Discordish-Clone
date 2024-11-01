3286e9c1b7d4cea7416e7d36cb86e43ab5fcc7bf
hewansirak <hewansg@gmail.com>
2024-11-01 22:33:04 +0300

    connecting convex and clerk

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,7 +2,12 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
- messages: defineTable({
+  users: defineTable({
+    username: v.string(),
+    image: v.string(),
+    clerkId: v.string(),
+  }).index("by_clerkId", ["clerkId"]),
+  messages: defineTable({
     sender: v.string(),
     content: v.string(),
   }),
