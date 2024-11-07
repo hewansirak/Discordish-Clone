d1d34d8e86d175c5b94c8034e21d319c53960985
hewansirak <hewansg@gmail.com>
2024-11-07 21:39:39 +0300

    possible fix

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
-"use client";
+'use client';
+
 import {
   Sidebar,
   SidebarContent,
@@ -11,19 +12,20 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarProvider,
-} from "@/components/ui/sidebar";
+} from '@/components/ui/sidebar';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { RedirectToSignIn, SignOutButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated, useQuery } from "convex/react";
-import { PlusIcon, User2Icon } from "lucide-react";
-import Link from "next/link";
-import { api } from "../../../convex/_generated/api";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+} from '@/components/ui/dropdown-menu';
+import { RedirectToSignIn, SignOutButton } from '@clerk/nextjs';
+import { Authenticated, Unauthenticated, useQuery } from 'convex/react';
+import { PlusIcon, User2Icon } from 'lucide-react';
+import Link from 'next/link';
+import { api } from '../../../convex/_generated/api';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
 export default function DashboardLayout({
   children,
 }: {
@@ -42,6 +44,7 @@ export default function DashboardLayout({
     </>
   );
 }
+
 function DashboardSidebar() {
   const user = useQuery(api.functions.user.get);
   if (!user) return null;
@@ -53,7 +56,7 @@ function DashboardSidebar() {
             <SidebarMenu>
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
-                  <Link href="/friends">
+                  <Link href='/friends'>
                     <User2Icon />
                     Friends
                   </Link>
@@ -63,8 +66,8 @@ function DashboardSidebar() {
           </SidebarGroupContent>
           <SidebarGroupLabel>Direct Messages</SidebarGroupLabel>
           <SidebarGroupAction>
-            <PlusIcon/>
-            <span className="sr-only">New Direct Message</span>
+            <PlusIcon />
+            <span className='sr-only'>New Direct Message</span>
           </SidebarGroupAction>
         </SidebarGroup>
       </SidebarContent>
@@ -75,12 +78,12 @@ function DashboardSidebar() {
               <SidebarMenuItem>
                 <DropdownMenu>
                   <DropdownMenuTrigger asChild>
-                    <SidebarMenuButton className="flex items-center gap-2">
-                      <Avatar className="size-6">
+                    <SidebarMenuButton className='flex items-center gap-2'>
+                      <Avatar className='size-6'>
                         <AvatarImage src={user.image} />
                         <AvatarFallback>{user.username[0]}</AvatarFallback>
                       </Avatar>
-                      <p className="font-medium">{user.username}</p>
+                      <p className='font-medium'>{user.username}</p>
                     </SidebarMenuButton>
                   </DropdownMenuTrigger>
                   <DropdownMenuContent>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,44 @@
-"use client";
+'use client';
 
 import {
-  Authenticated,
-  Unauthenticated,
   useMutation,
   useQuery,
-} from "convex/react";
-import { useState } from "react";
-import { api } from "../../convex/_generated/api";
-import { SignInButton } from "@clerk/nextjs";
+} from 'convex/react';
+import { useState } from 'react';
+import { api } from '../../convex/_generated/api';
 
 export default function Home() {
   const messages = useQuery(api.functions.message.list);
   const createMessage = useMutation(api.functions.message.create);
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    createMessage({ sender: "Hewan", content: input });
-    setInput("");
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    createMessage({ sender: 'Alice', content: input });
+    setInput('');
   };
 
   return (
-    <div>
-      {messages?.map((message, index) => (
-        <div key={index}>
-          <strong>{message.sender}</strong>
-          {message.content}
+    <>
+        <h1>Chat</h1>
+        <div>
+          {messages?.map((message, index) => (
+            <div key={index}>
+              <strong>{message.sender}: </strong>
+              {message.content}
+            </div>
+          ))}
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              name='message'
+              id='message'
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type='submit'>Send</button>
+          </form>
         </div>
-      ))}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="message"
-          id="message"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button type="submit">Send</button>
-      </form>
-    </div>
+    </>
   );
 }
